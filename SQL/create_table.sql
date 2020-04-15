4c1a03bc33e2eb3c9226aa15dd3b6ee0e6121433
thomascbyrne12 <47927482+thomascbyrne12@users.noreply.github.com>
2020-04-15 18:50:50 -0400

    Create create_table.sql

---
+++ SQL/create_table.sql
@@ -0,0 +1,18 @@
+DROP TABLE stock_info_data;
+
+CREATE TABLE stock_info_data (
+	trade_date DATE,
+	stock_open FLOAT,
+	high FLOAT,
+	low FLOAT,
+	stock_close FLOAT,
+	volume INT,
+	dividend INT,
+	split INT,
+	adj_open FLOAT,
+	adj_high FLOAT,
+	adj_low FLOAT,
+	adj_close FLOAT,
+	adj_volume FLOAT
+);
+);
