52711aad2ac67424419b7b86f0412b79965d1999
thomascbyrne12 <47927482+thomascbyrne12@users.noreply.github.com>
2020-04-16 15:34:29 -0400

    push

--- SQL/create_table.sql
+++ SQL/create_table.sql
@@ -1,17 +1,21 @@
+-- creates our table for stock trading data
+
 DROP TABLE stock_info_data;
 
 CREATE TABLE stock_info_data (
 	trade_date DATE,
-	stock_open FLOAT,
-	high FLOAT,
-	low FLOAT,
-	stock_close FLOAT,
+	stock_open FLOAT(32),
+	high FLOAT(32),
+	low FLOAT(32),
+	stock_close FLOAT(32),
 	volume INT,
 	dividend INT,
 	split INT,
-	adj_open FLOAT,
-	adj_high FLOAT,
-	adj_low FLOAT,
-	adj_close FLOAT,
+	adj_open FLOAT(32),
+	adj_high FLOAT(32),
+	adj_low FLOAT(32),
+	adj_close FLOAT(32),
 	adj_volume INT
 );
+
+SELECT * FROM stock_info_data;
